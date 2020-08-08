290d97239825d65ff83b2a6a4d1c966232cd690b
saha rick <jayitsaha@gmail.com>
2020-08-08 13:00:58 +0530

    CHATBOT V1 FEAT--ACHIEVED

---
+++ README.md
@@ -0,0 +1,32 @@
+# Ecommerce WebSite With Django2
+
+# Features
+	1. Full Authentication
+	2. Category Add
+	3. Product Add
+	4. Shopping Cart
+	5. Checkout Page
+	6. WIshlists Create
+	7. Wishlish Delete
+	8. Profile Crete
+	9. Profile Update
+
+# Future Plan
+	1.Online Payment Add
+	2. Shopkeeper feature
+
+
+## Quick Start Using Pipenv
+
+``` bash
+# Activate venv
+$ pipenv shell
+
+# Install dependencies
+$ pipenv install
+
+# For Static Folder
+$ python manage.py collectstatic
+
+```
+*currently you need to go to admin and add your image*
--- ecommerce/static/js/script_chat.js
+++ ecommerce/static/js/script_chat.js
@@ -17,9 +17,21 @@ $('.usrInput').on('keyup keypress', function (e) {
 });
 
 
+
+
+$('.nobg').on('click' , function (e){
+	e.preventDefault();
+	console.log("click");
+	send1();
+
+	return false;
+})
+
+
 //------------------------------------- Set user response------------------------------------
 function setUserResponse(val) {
 
+	console.log(val);
 
 	var UserResponse = '<img class="userAvatar" src=' + "./static/img/userAvatar.jpg" + '><p class="userMsg">' + val + ' </p><div class="clearfix"></div>';
 	$(UserResponse).appendTo('.chats').show('slow');
@@ -35,18 +47,20 @@ function scrollToBottomOfResults() {
 }
 
 function send(message) {
-	console.log("User Message:", message)
+	console.log("User Message:", )
 	$.ajax({
-		url: 'http://localhost:5005/webhooks/rest/webhook',
+		url: 'my-ajax-test-text/',
 		type: 'POST',
-		contentType: 'application/json',
-		data: JSON.stringify({
-			"message": message,
-			"sender": "username"
-		}),
+		// contentType: 'application/json',
+		// data: JSON.stringify({
+		// 	"message": message,
+		// 	"sender": "username"
+		// }),
+		data : {text: message},
 		success: function (data, textStatus) {
+			console.log(data);
 			setBotResponse(data);
-			console.log("Rasa Response: ", data, "\n Status:", textStatus)
+			console.log("Rasa Response: ", data);
 		},
 		error: function (errorMessage) {
 			setBotResponse("");
@@ -55,6 +69,33 @@ function send(message) {
 		}
 	});
 }
+
+function send1(message) {
+	// console.log("User Message:", message)
+	$.ajax({
+		url: 'my-ajax-test/',
+		type: 'POST',
+		// contentType: 'application/json',
+		// data: JSON.stringify({
+		// 	"message": message,
+		// 	"sender": "username"
+		// }),
+		data : {text: message},
+		success: function (data, textStatus) {
+			var data1 = JSON.parse(data);
+			console.log(data1['user']);
+			setUserResponse(data1['user']);
+			setBotResponse(data1['response']);
+			console.log("Rasa Response: ", data);
+		},
+		error: function (errorMessage) {
+			setBotResponse("");
+			console.log('Error' + errorMessage);
+
+		}
+	});
+}
+
 
 //------------------------------------ Set bot response -------------------------------------
 function setBotResponse(val) {
@@ -68,27 +109,29 @@ function setBotResponse(val) {
 
 		} else {
 			//if we get response from Rasa
-			for (i = 0; i < val.length; i++) {
-				//check if there is text message
-				if (val[i].hasOwnProperty("text")) {
-					var BotResponse = '<img class="botAvatar" src="./static/img/botAvatar.png"><p class="botMsg">' + val[i].text + '</p><div class="clearfix"></div>';
-					$(BotResponse).appendTo('.chats').hide().fadeIn(1000);
-				}
+			var BotResponse = '<img class="botAvatar" src="./static/img/botAvatar.png"><p class="botMsg">' + val + '</p><div class="clearfix"></div>';
+			$(BotResponse).appendTo('.chats').hide().fadeIn(1000);
+			// for (i = 0; i < val.length; i++) {
+			// 	//check if there is text message
+			// 	if (val[i].hasOwnProperty("text")) {
+			// 		var BotResponse = '<img class="botAvatar" src="./static/img/botAvatar.png"><p class="botMsg">' + val[i].text + '</p><div class="clearfix"></div>';
+			// 		$(BotResponse).appendTo('.chats').hide().fadeIn(1000);
+			// 	}
 
-				//check if there is image
-				if (val[i].hasOwnProperty("image")) {
-					var BotResponse = '<div class="singleCard">' +
-						'<img class="imgcard" src="' + val[i].image + '">' +
-						'</div><div class="clearfix">'
-					$(BotResponse).appendTo('.chats').hide().fadeIn(1000);
-				}
+			// 	//check if there is image
+			// 	if (val[i].hasOwnProperty("image")) {
+			// 		var BotResponse = '<div class="singleCard">' +
+			// 			'<img class="imgcard" src="' + val[i].image + '">' +
+			// 			'</div><div class="clearfix">'
+			// 		$(BotResponse).appendTo('.chats').hide().fadeIn(1000);
+			// 	}
 
-				//check if there is  button message
-				if (val[i].hasOwnProperty("buttons")) {
-					addSuggestion(val[i].buttons);
-				}
+			// 	//check if there is  button message
+			// 	if (val[i].hasOwnProperty("buttons")) {
+			// 		addSuggestion(val[i].buttons);
+			// 	}
 
-			}
+			// }
 			scrollToBottomOfResults();
 		}
 
@@ -132,5 +175,5 @@ $(document).on("click", ".menu .menuChips", function () {
 	console.log("button payload: ",this.getAttribute('data-payload'))
 	setUserResponse(text);
 	send(payload);
-	$('.suggestions').remove(); //delete the suggestions 
+	$('.suggestions').remove(); //delete the suggestions
 });
